f3388e85301b46aa8b4410eb545118175f4955d5
JULIA LOPES DA SILVA FERNANDES <julia.sfernandes@aluno.senac.rio>
2026-05-27 21:12:51 -0300

    ex16.py

---
+++ ex16.py
@@ -0,0 +1,8 @@
+temperatura = float ( input("digite a sua temperatura"))
+if temperatura < 18:
+    print("o tempo esta frio")
+elif temperatura >=18 and temperatura <= 30:
+    print ("o tempo esta agradavel")
+elif temperatura > 30:
+    print("o tempo esta calor")
+    
